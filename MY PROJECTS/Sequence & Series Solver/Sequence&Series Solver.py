f5bcbde07a8f499f9a62f32e05d4e250a436d31e
MahdeenSky <islammahdeen@gmail.com>
2020-02-27 17:40:23 +0300

    Sequence and Series Solver
    Updated, more efficient reduced from 150 lines to 120 lines of code

--- MY PROJECTS/Sequence & Series Solver/Sequence&Series Solver.py
+++ MY PROJECTS/Sequence & Series Solver/Sequence&Series Solver.py
@@ -1,76 +1,43 @@
-# just to make the input_checker function smaller and to eliminate repeated code
+# just to make the input_checker function smaller and to eliminate repeated code, responsible for iterating through a list of inputs
 def three_variables_looper_arithmetic():
-    a = input("enter a1: ")
-    b = input("enter n: ")
-    c = input("enter d: ")
-    original = 0
-    count_list = [[a], [b], [c]]
-    loop = 0
-    for count in count_list:
-        if isinstance(count[0], str):
-            original = float(eval(count[0]))
-            count_list[loop][0] = original
-        loop += 1
-    return count_list[0][0], count_list[1][0], count_list[2][0]
+    count_list = [input("enter a1: "), input("enter n: "), input("enter d: ")]
+    count_list = [float(eval(count)) for count in count_list if isinstance(count, str)]
+    return count_list
 
-# just to make the input_checker function smaller and to eliminate repeated code
+# just to make the input_checker function smaller and to eliminate repeated code, responsible for iterating through a list of inputs
 def three_variables_looper_geometric():
-    a = input("enter a1: ")
-    b = input("enter r: ")
-    c = input("enter n: ")
-    original = 0
-    count_list = [[a], [b], [c]]
-    loop = 0
-    for count in count_list:
-        if isinstance(count[0], str):
-            original = float(eval(count[0]))
-            count_list[loop][0] = original
-        loop += 1
-    return count_list[0][0], count_list[1][0], count_list[2][0]
+    count_list = [input("enter a1: "), input("enter r: "), input("enter n: ")]
+    count_list = [float(eval(count)) for count in count_list if isinstance(count, str)]
+    return count_list
 
 
 # loops through all the inputs of a given situation based on whether its arithmetic
 # or not, and checks whether the input is string like "6/2" so it could evaluate it, allows input of fractions
-def input_checker(arithmetic_1_geometric_2, formula_num, L):
-    if arithmetic_1_geometric_2 == 1:
-        if formula_num == 1:
+def input_checker(choice_main, choice_sub, L):
+    if choice_main == 'arithmetic':
+        if choice_sub == 'a_nth':
             return three_variables_looper_arithmetic()
 
 
-        elif formula_num == 2:
+        elif choice_sub == 'sum_to_nth_without_L':
             return three_variables_looper_arithmetic()
 
-        elif formula_num == 3:
-            a1 = input("enter a1: ")
-            b = input("enter n: ")
-            c = input("enter d: ")
-            original = 0
-            count_list = [[a1], [b], [c], [L]]
-            loop = 0
-            for count in count_list:
-                if isinstance(count[0], str):
-                    original = float(eval(count[0]))
-                    count_list[loop][0] = original
-            return count_list[0][0], count_list[1][0], count_list[2][0], count_list[3][0]
+        elif choice_sub == 'sum_to_nth_with_L':
+            count_list = [input("enter a1: "), input("enter n: "), L]
+            count_list = [float(eval(count)) for count in count_list if isinstance(count, str)]
+            return count_list
             
-    elif arithmetic_1_geometric_2 == 2:
-        if formula_num == 1:
+    elif choice_main == 'geometric':
+        if choice_sub == 'a_nth':
             return three_variables_looper_geometric()
         
-        elif formula_num == 2:
+        elif choice_sub == 'sum_to_nth':
             return three_variables_looper_geometric()
 
-        elif formula_num == 3:
-            a1 = input("enter a1: ")
-            b = input("enter n: ")
-            original = 0
-            count_list = [[a1], [b]]
-            loop = 0
-            for count in count_list:
-                if isinstance(count[0], str):
-                    original = float(eval(count[0]))
-                    count_list[loop][0] = original
-            return count_list[0][0], count_list[1][0]
+        elif choice_sub == 'sum_to_infinity':
+            count_list = [input("enter a1: "), input("enter r: ")]
+            count_list = [float(eval(count)) for count in count_list if isinstance(count, str)]
+            return count_list
 
 # as this code is for a calculator the a and b buttons are right beside each other, so after you find your desired result
 # you enter a to stop and b to continue 
@@ -93,61 +60,63 @@ print("Sequence & Series Solver")
 
 # asks whether you want to solve arithmetically or geometrically, depends on the sequence/series
 while True:
-    choice = ""
-    choices = ["a", "b"]
-    while choice not in choices:
-        choice = input("a for arithmetic\nb for geometric\n>> ")
+    choice_main , choice_input_main = None, None
+    choices_main = {'a','b'}
+    choice_map_main ={"a": 'arithmetic', "b": 'geometric'}
+    while choice_input_main not in choices_main:
+        choice_input_main = input("a for arithmetic\nb for geometric\n>> ")
+        choice_main = choice_map_main[choice_input_main]
 
-    if choice == "a":
-        arithmetic_1_geometric_2 = 1
-        choice_2 = input("a for a_nth term\nb for sum\n>> ")
+    if choice_main == "arithmetic":
+        choice_sub, choice_input_sub = None, None
+        choices_sub = {'a', 'b'}
+        choice_map_sub = {'a': 'a_nth', 'b': 'sum_to_nth_without_L', 'c': 'sum_to_nth_with_L'}
+        choice_input_sub = input("a for a_nth term\nb for sum\n>> ")
+        choice_sub = choice_map_sub[choice_input_sub]
 
-        # the variable arithmetic_1_geometric_2 refers to whether the inputs are for arithmetic hence it is 1, or the inputs
-        # are for geometric hence 2
-        # formula_num refers to the types of formulas you'll use in sequences/series
-        if choice_2 == "a":
+        # the variable choice_main refers to whether the choice is arithmetic or geometric
+        # choice_sub refers to the types of formulas you'll use in sequences/series
+        if choice_sub == "a_nth":
             print("a_nth=a1+(n-1)d")
-            formula_num = 1
-            a1, n, d = input_checker(arithmetic_1_geometric_2, formula_num, 0)
+            a1, n, d = input_checker(choice_main, choice_sub, 0)
             result = a1+(n-1)*d
             print(result)
 
-        elif choice_2 == "b":
+        elif choice_sub == "sum_to_nth_without_L":
             print("Sn=(n/2)(2a1+(n-1)d)\nSn=(n/2)(a1+L)\nEnter x if L is unknown")
             L = input("Enter L: ")
             if L == "x":
-                formula_num = 2
-                a1, n, d = input_checker(arithmetic_1_geometric_2, formula_num, L)
+                a1, n, d = input_checker(choice_main, choice_sub, L)
                 result = (n/2)*(2*a1+(n-1)*d)
                 print(result)
             else:
-                formula_num = 3
-                a1, n, d, L = input_checker(arithmetic_1_geometric_2, formula_num, L)
+                choice_sub = choice_map_sub['c']
+                a1, n, L = input_checker(choice_main, choice_sub, L)
                 result = (n/2)*(a1+L)
                 print(result)
-    
-    elif choice == "b":
-        arithmetic_1_geometric_2 = 2
-        choice_2 = input("a for a_nth term\nb for sum\nc for sum_to_inf")
 
-        if choice_2 == "a":
+    elif choice_main == "geometric":
+        choice_sub, choice_input_sub = None, None
+        choices_sub = {'a', 'b'}
+        choice_map_sub = {'a': 'a_nth', 'b': 'sum_to_nth', 'c': 'sum_to_infinity'}
+        choice_input_sub = input("a for a_nth term\nb for sum\nc for sum to infinity\n>> ")
+        choice_sub = choice_map_sub[choice_input_sub]
+
+        if choice_sub == "a_nth":
             print("a_nth=a1(r)^(n-1)")
-            formula_num = 1
-            a1, r, n = input_checker(arithmetic_1_geometric_2, formula_num, 0)
+            a1, r, n = input_checker(choice_main, choice_sub, 0)
             result = a1*(r)**(n-1)
             print(result)
 
-        elif choice_2 == "b":
+        elif choice_sub == "sum_to_nth":
             print("Sn=(a1(1-(r)^n))/(1-r)")
-            formula_num = 2
-            a1, r, n = input_checker(arithmetic_1_geometric_2, formula_num, 0)
-            result = (a1(1-(r)**n))/(1-r)
+            a1, r, n = input_checker(choice_main, choice_sub, 0)
+            result = (a1*(1-(r)**n))/(1-r)
             print(result)
 
-        elif choice_2 == "c":
-            print("S_inf=a1/(1/r)")
-            formula_num = 3
-            a1, r = input_checker(arithmetic_1_geometric_2, formula_num, 0)
+        elif choice_sub == "sum_to_infinity":
+            print("S_inf=a1/(1-r)")
+            a1, r = input_checker(choice_main, choice_sub, 0)
             result = a1/(1-r)
             print(result)
     
