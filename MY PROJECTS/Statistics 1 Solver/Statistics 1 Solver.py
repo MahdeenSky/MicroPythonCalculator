4febb9c9fecd193696e44b400482292a28373033
MahdeenSky <islammahdeen@gmail.com>
2020-04-19 19:21:51 +0300

    Update Statistics 1 Solver.py

--- MY PROJECTS/Statistics 1 Solver/Statistics 1 Solver.py
+++ MY PROJECTS/Statistics 1 Solver/Statistics 1 Solver.py
@@ -16,14 +16,13 @@ def interpolation_grouped_data(grouped_data, cumulative_frequencies, position): 
                 mid_cu_freq = position
                 interval_index = index + 1
                 break
-    lower_bound = grouped_data[interval_index][0]
-    higher_bound = grouped_data[interval_index][1]
-    return interpolation([mn_cu_freq, mid_cu_freq, mx_cu_freq, lower_bound, higher_bound])
+    lower_bound, upper_bound = grouped_data[interval_index][0:2]
+    return interpolation([mn_cu_freq, mid_cu_freq, mx_cu_freq, lower_bound, upper_bound])
 
 
 def interpolation(data_for_interpolation): # uses interpolation to find the result, cu represents cumulative
-    mn_cu_freq, mid_cu_freq, mx_cu_freq, lower_bound, higher_bound = data_for_interpolation
-    result = lower_bound + ( ( (mid_cu_freq - mn_cu_freq)/(mx_cu_freq - mn_cu_freq) ) * (higher_bound - lower_bound) )
+    mn_cu_freq, mid_cu_freq, mx_cu_freq, lower_bound, upper_bound = data_for_interpolation
+    result = lower_bound + ( ( (mid_cu_freq - mn_cu_freq)/(mx_cu_freq - mn_cu_freq) ) * (upper_bound - lower_bound) )
     return result
 
 
@@ -34,10 +33,10 @@ def listed_data_stats(listed_data): # for dealing with listed data Ex: 1,2,3,4 o
     mean = sum_listed_data / number_of_data
 
     # sum of each data squared
-    sum_squared_listed_data = sum([i**2 for i in listed_data])
+    sum_squared_listed_data = sum(i**2 for i in listed_data)
 
     # variance, and standard deviation
-    variance = (sum_squared_listed_data / number_of_data) - (mean)**2
+    variance = (sum_squared_listed_data / number_of_data) - mean**2
     standard_deviation = round(math.sqrt(variance), 5)
 
     # median
@@ -51,10 +50,10 @@ def listed_data_stats(listed_data): # for dealing with listed data Ex: 1,2,3,4 o
         median = round(sorted_listed_data[number_of_data//2], 5)
 
     # mode
-    m = max([listed_data.count(value) for value in listed_data])
-    mode = set([str(x) for x in listed_data if listed_data.count(x) == m]) if m>1 else None
+    m = max(listed_data.count(value) for value in listed_data)
+    mode = set(str(x) for x in listed_data if listed_data.count(x) == m) if m>1 else None
 
-    return sum_listed_data, sum_squared_listed_data, number_of_data, mean, median, mode, round(variance, 5), round(standard_deviation, 5)
+    return [sum_listed_data, sum_squared_listed_data, number_of_data, mean, median, mode, round(variance, 5), round(standard_deviation, 5)]
 
 
 def grouped_data_stats(grouped_data): # for dealing with grouped data ex: [[lower bound, upper bound, frequency], [...], [...]] etc. in [[0, 10, 16], [10, 15, 18], [15, 20, 50]] in the first list, 0 and 10 represents the interval 0 -> 10, and 16 is the frequency of numbers in this range
@@ -63,7 +62,7 @@ def grouped_data_stats(grouped_data): # for dealing with grouped data ex: [[lowe
     sum_x = 0
     sum_x_squared = 0
     number_of_data = 0
-    if grouped_data[1][0] - grouped_data[0][1] != 0: # if there are gaps in data
+    if grouped_data[1][0] != grouped_data[0][1]: # if there are gaps in data
         gap = (grouped_data[1][0] - grouped_data[0][1])/2
         for data in grouped_data:
             if data[0] != 0:
@@ -84,7 +83,7 @@ def grouped_data_stats(grouped_data): # for dealing with grouped data ex: [[lowe
     mean = sum_x / number_of_data 
 
     # variance, and standard deviation
-    variance = (sum_x_squared / number_of_data) - (sum_x / number_of_data)**2 # 
+    variance = (sum_x_squared / number_of_data) - mean**2
     standard_deviation = math.sqrt(variance)
 
     # lower quartile, median, and upper quartile, and interquartile range
@@ -93,16 +92,16 @@ def grouped_data_stats(grouped_data): # for dealing with grouped data ex: [[lowe
     upper_quartile = interpolation_grouped_data(grouped_data, cumulative_frequencies, (75/100) * number_of_data)
     interquartile_range = upper_quartile - lower_quartile
 
-    return sum_x, sum_x_squared, number_of_data, mean, variance, standard_deviation, lower_quartile, median, upper_quartile, interquartile_range
+    return [sum_x, sum_x_squared, number_of_data, mean, variance, standard_deviation, lower_quartile, median, upper_quartile, interquartile_range]
     
         
 def statistics(): # checks for what you want
     choice = input("a for\nInterpolation\nb for\nListed Data\nc for Grouped Data\n: ")
 
     if choice == "a": # interpolation
-        mn_cu_freq = mid_cu_freq = mx_cu_freq = lower_bound = higher_bound = None
-        variables = [mn_cu_freq, mid_cu_freq, mx_cu_freq, lower_bound, higher_bound] # values to be inputted for interpolation
-        variables_names = ["mn_cu_freq", "mid_cu_freq", "mx_cu_freq", "lower_bound", "higher_bound"]
+        mn_cu_freq = mid_cu_freq = mx_cu_freq = lower_bound = upper_bound = None
+        variables = [mn_cu_freq, mid_cu_freq, mx_cu_freq, lower_bound, upper_bound] # values to be inputted for interpolation
+        variables_names = ["mn_cu_freq", "mid_cu_freq", "mx_cu_freq", "lower_bound", "upper_bound"]
         for index, _ in enumerate(variables): 
             variables[index] = float(input("Enter {}: ".format(variables_names[index])))
         print("x = ", interpolation(variables))
@@ -115,8 +114,7 @@ def statistics(): # checks for what you want
                 break
             value = int(value)
             listed_data.append(value)
-        results.extend(listed_data_stats(listed_data))
-        results = [str(value) for value in results]
+        results = [str(value) for value in listed_data_stats(listed_data)]
         print("", "Sum_x = " + results[0], "Sum_x^2 = " + results[1], "n = " + results[2], "Mean = " + results[3], "Median = " + results[4],
         "Mode = " + results[5], "Variance = " + results[6], "Standard_Deviation = " + results[7], sep="\n")
 
@@ -129,8 +127,7 @@ def statistics(): # checks for what you want
             end_boundary = input("End Bound: ")
             frequency = input("Frequency: ")
             grouped_data.append([int(start_boundary), int(end_boundary), int(frequency)]) # each row in the grouped data is a list
-        results.extend(grouped_data_stats(grouped_data))
-        results = [str(round(value, 5)) for value in results]
+        results = [str(round(value, 5)) for value in grouped_data_stats(grouped_data)]
         print("", "Sum_x = " + results[0], "Sum_x^2 = " + results[1], "n = " + results[2], "Mean = " + results[3], "Variance = " + results[4],
         "Standard Deviation = " + results[5], "Lower Quartile = " + results[6], "Median = " + results[7], "Upper Quartile = " + results[8],
          "IQR = " + results[9], sep="\n")
