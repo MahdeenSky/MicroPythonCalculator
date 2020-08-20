f3e97fae8acafc7891b8335758b3226a978ed111
MahdeenSky <islammahdeen@gmail.com>
2020-08-20 16:23:10 +0300

    20/08/2020

--- Quadratic Solver&Factorer/Quadratic Polynomial Factorer and Solver v2.py
+++ Quadratic Solver&Factorer/Quadratic Polynomial Factorer and Solver v2.py
@@ -1,248 +1,135 @@
-from math import sqrt, pi
+# quadratic factorer, and solver
+from math import sqrt
 
-print("ax^2+bx+c=0")
-
-def input_pi_replacer(prompt):
-    return float(eval(input(prompt).replace("pi", str(pi))))
-
-def stopper():
-    stop_or_continue = input("Stop?: ")
-    if stop_or_continue == "x":
-        raise SystemExit  
-
-
-def polynomial_checker(a, b, c):
-    negative_factor = False
-    if a < 0:
-        negative_factor = True
-    #  checks if gcf can be applied to simplify the quadratic expression
-    b_divisible_by_a = (b % a == 0)
-    c_divisible_by_a = (c % a == 0)
-    check_divisible = (b_divisible_by_a and c_divisible_by_a)
-    a_is_one = (a == (a / a))
-    return a_is_one, check_divisible, negative_factor
-
-
-def isclose(a, b, tol):
-    if abs(a-b) <= tol:
-        return True
-    else:
-        return False
-
-
-def gcd(x, y):
-    while y != 0:
-        (x, y) = (y, x % y)
+def gcd(*values):
+    x, *b = values
+    for y in b:
+        while y != 0:
+            (x, y) = (y, x % y)
     return x
 
 
-def fraction(a):
-    factor = 0
+def isclose(a, b, tolerance):
+    return abs(a-b) <= tolerance
+
+
+def fraction(a, factor=0):
     while True:
         factor += 1
         a_rounded = int(round(a*factor))
         if isclose(a*factor, a_rounded, 0.01):
-            text = ("{}/{}".format(a_rounded, factor))
-            return text
+            break
+    if factor == 1:
+        return a_rounded
+    else:
+        return "{}/{}".format(a_rounded, factor)
 
 
 def simplify_fraction(numer, denom):
     if denom == 0:
-        return "Division by 0 - result undefined"
+        return None, None
 
     # Remove greatest common divisor:
     common_divisor = gcd(numer, denom)
-    (reduced_num, reduced_den) = (numer / common_divisor, denom / common_divisor)
+    reduced_num, reduced_den = numer / common_divisor, denom / common_divisor
     # Note that reduced_den > 0 as documented in the gcd function.
 
     if common_divisor == 1:
-        return (numer, denom)
+        return numer, denom
     else:
         # Bunch of nonsense to make sure denominator is negative if possible
-        if (reduced_den > denom):
+        if reduced_den > denom:
             if (reduced_den * reduced_num < 0):
-                return (-reduced_num, -reduced_den)
+                return -reduced_num, -reduced_den
             else:
-                return (reduced_num, reduced_den)
+                return reduced_num, reduced_den
         else:
-            return (reduced_num, reduced_den)
+            return reduced_num, reduced_den
 
 
-def complete_the_square(a, b, c, output):
-    a_original, b_original, c_original = float(a), float(b), float(c)
-    a_is_one, check_divisible, negative_factor = polynomial_checker(a_original, b_original, c_original)
-    if negative_factor:
-        gcf = -int(gcd(int(-a_original), gcd(int(-b_original), int(-c_original))))
-        b_original, c_original, a_original = -b_original, -c_original, -a_original
+def get_determinant(a, b, c):
+    return b**2 - 4*a*c
 
-    if a_is_one and (check_divisible is False):
-        gcf = ""
 
-    elif check_divisible and (a_is_one is False):
-
-        if negative_factor is False:
-            gcf = int(gcd(int(a_original), gcd(int(b_original), int(c_original))))
-        b_original, c_original, a_original = b_original / a_original, c_original / a_original, a_original / a_original
-
+def solve_quadratic_equation(a, b, c):
+    determinant = get_determinant(a, b, c)
+    # two solutions, or one solution
+    if determinant >= 0:
+        return ( (-b+sqrt(get_determinant(a, b, c))) / (2*a), (-b-sqrt(get_determinant(a, b, c))) / (2*a)) # (x1, x2)
+    # no solutions
     else:
-        gcf = ""
-        b_original, c_original, a_original = b_original/a_original, c_original/a_original, a_original/a_original
-    d = b_original / (2 * a_original)
-    e = c_original - (b_original**2/(4 * a_original))
-
-    d_eval = False
-    e_eval = False
-    if d > 0:
-        sign1 = "+"
-        if (d % 1 != 0):
-            d = fraction(d)
-            d_eval = True
-    else:
-        if (d % 1 != 0):
-            d = fraction(-d)
-            d_eval = True
-        sign1 = "-"
-        if d_eval is False:
-            d = -d
-
-    if e > 0:
-        sign2 = "+"
-        if (e % 1 != 0):
-            e = fraction(e)
-            e_eval = True
-    else:
-        if (e % 1 != 0):
-            e = fraction(-e)
-            e_eval = True
-        sign2 = "-"
-        if e_eval is False:
-            e = -e
-
-    if output:
-        print("The Factored Form is:\n{}(x{}{})^2{}{}".format(gcf, sign1, d, sign2, e))
-    if d_eval:
-        d = -eval(d)
-    elif d_eval is False:
-        pass
-    if e_eval:
-        e = -eval(e)
-    elif e_eval is False:
-        e = -e
-
-    return gcf, d, e
+        return None
 
 
-def quadratic_function(a, b, c, output):
-    complete_square = False
+def factor_quadratic_equation(a, b, c):
     a_original, b_original, c_original = a, b, c
-    if b ** 2 - 4 * a * c >= 0:
-        a_is_one, check_divisible, negative_factor = polynomial_checker(a, b, c)
-        if negative_factor:
-            gcf = -int(gcd(int(-a), gcd(int(-b), int(-c))))
-            b, c, a = -b, -c, -a
-        if a_is_one or (check_divisible is False):
+
+    if get_determinant(a, b, c) >= 0:
+        expression_is_divisible = ((b % a == 0) and (c % a == 0))
+
+        gcf = gcd(a, b, c)
+        if a < 0:
+            gcf = -gcf
+            a, b, c = -a, -b, -c
+
+        if a == 1 or not expression_is_divisible:
             gcf = ""
-        elif check_divisible and (a_is_one is False):
-            if negative_factor is False:
-                gcf = int(gcd(int(a), gcd(int(b), int(c))))
-            b, c, a = b/a, c/a, a/a
-        x1 = (-b + sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-        x2 = (-b - sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-        # Added a "-" to these next 2 values because they would be moved to the other side of the equation
-        mult1 = -x1 * a
-        mult2 = -x2 * a
-        (num1, den1) = simplify_fraction(a, mult1)
-        (num2, den2) = simplify_fraction(a, mult2)
-        if (num1 > a) or (num2 > a):
-            # acquires the complete square since it is not factorable using normal means
-            if output:
-                complete_the_square(a_original, b_original, c_original, True)
-            complete_square = True
-            gcf, d, e = complete_the_square(a_original, b_original, c_original, False)
-        else:
-            # Getting ready to make the print look nice
-            if output:
-                if (den1 > 0):
-                    sign1 = "+"
-                else:
-                    sign1 = ""
-                if (den2 > 0):
-                    sign2 = "+"
-                else:
-                    sign2 = ""
-                print("The Factored Form is:\n{}({}x{}{})({}x{}{})".format(gcf, int(num1), sign1, int(den1), int(num2), sign2,int(den2)))
-    else:
-        # if the part under the sqrt is negative, you have a solution with i
+        elif expression_is_divisible and not (a == 1):
+            a, b, c = a/a, b/a, c/a
 
-        if output:
-            complete_the_square(a_original, b_original, c_original, True)
-            print("Solutions are imaginary")
-        complete_square = True
-        gcf, d, e = complete_the_square(a_original, b_original, c_original, False)
-    if complete_square:
-        return complete_square, gcf, d, e
-    return complete_square
+        x1, x2 = solve_quadratic_equation(a, b, c)
+        mult1, mult2 = -x1*a, -x2*a
+        num1, den1 = simplify_fraction(a, mult1)
+        num2, den2 = simplify_fraction(a, mult2)
+
+        if (num1 > a) or (num2 > a): # then complete square 2(x+3) + 1
+            # a(x+p)^2 + q
+            a, b, c = a_original, b_original, c_original
+            p = b/(2*a)
+            q = c - (b**2)/(4*a)
+            sign1 = "+" if p > 0 else "-"
+            sign2 = "+" if q > 0 else "-"
+            p = -p if sign1 == "-" else p
+            q = -q if sign2 == "-" else q
+
+            return "{}(x{}{})^2 {} {}".format(a, sign1, fraction(p), sign2, fraction(q))
+
+        else: # normal factoring (x+3)(x+3) or 2(x+3)(x+3)
+            sign1 = "+" if den1 > 0 else ""
+            sign2 = "+" if den2 > 0 else ""
+            return "{}({}x{}{})({}x{}{})".format(gcf, fraction(num1), sign1, fraction(den1), fraction(num2), sign2, fraction(den2))
+
+    else: # no factored form
+        return None 
 
 
-def solutions(a, b, c, complete_square):
-    if complete_square is False:
-        discriminant = b ** 2 - 4 * a * c
-        if discriminant == 0:
-            x_one = (-b + sqrt(b ** 2 - 4 * a * c)) / (2 * a)  # x_one
-            if eval(fraction(x_one)) % 1 == 0:
-                print("x1 is : ", int(x_one))
-            else:
-                print("x1 is : ", fraction(x_one))
-        if discriminant > 0:
-            x_one = (-b + sqrt(b ** 2 - 4 * a * c)) / (2 * a)  # x_one
-            if eval(fraction(x_one)) % 1 == 0:
-                print("x1 is : ", int(x_one))
-            else:
-                print("x1 is : ", fraction(x_one))
-            x_two = (-b - sqrt(b ** 2 - 4 * a * c)) / (2 * a)  # x_two
-            if eval(fraction(x_two)) % 1 == 0:
-                print("x2 is : ", int(x_two))
-            else:
-                print("x2 is : ", fraction(x_two))
-        if discriminant < 0:
-            pass
-    elif complete_square:
-        gcf, d, e = a, b, c
-        right_side = -e
-        if gcf == "":
-            gcf = 1
-        right_side = right_side/gcf
-        if (right_side % 1 != 0):
-            right_side_3 = fraction(right_side)
-        else:
-            right_side_3 = right_side
-        right_side_1 = sqrt(right_side)
-        right_side_2 = -sqrt(right_side)
-        x_one = right_side_1 - d
-        x_two = right_side_2 - d
-        sign_one = "+"
-        sign_two = "-"
-        print("x1 is : {}{}sqrt({})/{} or {} ".format(-d, sign_one, right_side_3, gcf, round(x_one, 3)))
-        print("x2 is : {}{}sqrt({})/{} or {} ".format(-d, sign_two, right_side_3, gcf, round(x_two, 3)))
-
-
-stop_flag = False
 while True:
-    try:
-        if stop_flag is True:
-            break
+    a = int(input("insert a: "))
+    b = int(input("insert b: "))
+    c = int(input("insert c: "))
+    factored_form = factor_quadratic_equation(a, b, c)
+    solutions = solve_quadratic_equation(a, b, c)
 
-        a = input_pi_replacer("insert a: ")
-        b = input_pi_replacer("insert b: ")
-        c = input_pi_replacer("insert c: ")
+    print(factored_form) if factored_form else print("No Factored Form")
 
-        complete_square = quadratic_function(a, b, c, True)
-        if complete_square:
-            complete_square, a, b, c = quadratic_function(a, b, c, False)
+    if solutions:
+        print("x1 = {}".format(round(solutions[0], 5)))
+        if solutions[0] != solutions[1]:
+            print("x2 = {}".format(round(solutions[1], 5)))
+    else:
+        print("No Solution")
 
-        solutions(a, b, c, complete_square)
-        stopper()
+    stop = input("'x' to stop: ")
+    if stop == 'x':
+        break
+         
 
-    except ValueError:
-        stopper()
-        pass
+
+
+    
+
+
+
+
+
+
--- Suvat Calculator V2/Suvat Calculator V2.py
+++ Suvat Calculator V2/Suvat Calculator V2.py
@@ -3,143 +3,113 @@
 # s = 0.5(u+v)*t
 # v^2 = u^2 + 2a*s
 # s = ut + 0.5at^2
-from math import sqrt, pi
-
-# replaces "pi" with the symbol of pi if inputted from a calculator
-def input_pi_replacer(prompt):
-    return input(prompt).replace("pi", str(pi))
-
+from math import sqrt
 
 def stopper():
-    stop_or_continue = input("Stop?: then enter 'x'")
+    stop_or_continue = input("Stop?: then enter 'x'\n: ")
     if stop_or_continue == "x":
         raise SystemExit  
 
-print("Suvat Calculator:")
-print("if variable not\ngiven then type\n'x'\n")
+
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
 
 
 def suvatsolver():
 
-    while True:
-        # this will keep track of how many variables are known, since three are required.
-        variable_count = 0
-        [s_known, u_known, v_known, a_known, t_known] = [False, False, False, False, False]
+    # this will keep track of how many variables are known, since three are required.
+    variables = {'distance': None, 'initial_velocity': None, 'final_velocity': None, 'acceleration': None, 'time': None}
 
-        distance = input_pi_replacer("Distance in metres\n:  ")
-        # this assumes that the input is actually acceleration number.
-        # then it says the variable is known and adds 1 to the variable count.
-        if distance != "x":
-            s_known = True
-            distance = float(eval(distance))
-            variable_count += 1
+    variables['distance']         = input(("Distance in metres\n: "))
+    variables['initial_velocity'] = input(("Initial velocity in m/s\n: "))
+    variables['final_velocity']   = input(("Final velocity in m/s\n: "))
+    variables['acceleration']     = input(("Acceleration in m/s^2\n: "))
+    variables['time']             = input(("Time in seconds\n: "))
 
-        initial_velocity = input_pi_replacer("Initial velocity\nin metres/second\n: ")
-        if initial_velocity != "x":
-            u_known = True
-            initial_velocity = float(eval(initial_velocity))
-            variable_count += 1
+    for var_name, value in variables.items():
+        if isfloat(value):
+            variables[var_name] = float(value)
+        else:
+            variables[var_name] = None
 
-        final_velocity = input_pi_replacer("Final velocity\n in metres/second\n: ")
-        if final_velocity != "x":
-            v_known = True
-            final_velocity = float(eval(final_velocity))
-            variable_count += 1
+    if sum(1 if value != None else 0 for value in variables.values()) < 3:
+        print("\nNot Enough Variables Known!")
+        return    
+    print()
 
-        acceleration = input_pi_replacer("Acceleration\n in metres/second squared\n: ")
-        if acceleration != "x":
-            a_known = True
-            acceleration = float(eval(acceleration))
-            variable_count += 1
+    # this next bunch of statements pretty much goes through
+    # all possible combinations of three variables
+    # and calculates the remaining unknowns (I solved the equations myself)
+    if variables['distance'] and variables['initial_velocity'] and variables['final_velocity']:
+        variables['acceleration'] = (variables['final_velocity'] ** 2 - variables['initial_velocity'] ** 2) / (2 * variables['distance'])
+        variables['time'] = 2 * variables['distance'] / (variables['initial_velocity'] + variables['final_velocity'])
+        print("Acceleration =\n", variables['acceleration'])
+        print("Time =\n", variables['time'])
 
-        time = input_pi_replacer("Time\nin seconds\n: ")
-        if time != "x":
-            t_known = True
-            time = float(eval(time))
-            variable_count += 1
+    elif variables['distance'] and variables['initial_velocity'] and variables['acceleration']:
+        variables['final_velocity'] = sqrt(variables['initial_velocity'] ** 2 + 2 * variables['acceleration'] * variables['distance'])
+        t1 = -variables['initial_velocity'] / variables['acceleration'] + sqrt(2 * variables['acceleration'] * variables['distance'] + variables['initial_velocity'] ** 2) / variables['acceleration']
+        t2 = -variables['initial_velocity'] / variables['acceleration'] - sqrt(2 * variables['acceleration'] * variables['distance'] + variables['initial_velocity'] ** 2) / variables['acceleration']
+        print("Final velocity =\n", variables['final_velocity'], "\nor ", -variables['final_velocity'])
+        print("Time =\n", t1, "\nor ", t2)
 
-        if variable_count < 3:
-            print("not enough variables")
-            break
+    elif variables['distance'] and variables['initial_velocity'] and variables['time']:
+        variables['final_velocity'] = 2 * variables['distance'] / variables['time'] - variables['initial_velocity']
+        variables['acceleration'] = 2 * (variables['distance'] - variables['initial_velocity'] * variables['time']) / variables['time'] ** 2
+        print("Final velocity =\n", variables['final_velocity'])
+        print("Acceleration =\n", variables['acceleration'])
 
-        # this next bunch of statements pretty much goes through
-        # all possible combinations of three variables
-        # and calculates the remaining unknowns (I solved the equations myself)
-        if s_known and u_known and v_known:
-            acceleration = (final_velocity ** 2 - initial_velocity ** 2) / (2 * distance)
-            time = 2 * distance / (initial_velocity + final_velocity)
-            print("")
-            print("Acceleration =\n", acceleration)
-            print("Time =\n", time)
+    elif variables['distance'] and variables['final_velocity'] and variables['acceleration']:
+        variables['initial_velocity'] = sqrt(variables['final_velocity'] ** 2 - 2 * variables['acceleration'] * variables['distance'])
+        t1 = 2 * variables['distance'] / (variables['final_velocity'] + sqrt(variables['final_velocity'] ** 2 - 2 * variables['acceleration'] * variables['distance']))
+        t2 = 2 * variables['distance'] / (variables['final_velocity'] - sqrt(variables['final_velocity'] ** 2 - 2 * variables['acceleration'] * variables['distance']))
+        print("Initial velocity =\n", variables['initial_velocity'], "\nor", -variables['initial_velocity'])
+        print("Time =\n", t1, "\nor", t2)
 
-        elif s_known and u_known and a_known:
-            final_velocity = sqrt(initial_velocity ** 2 + 2 * acceleration * distance)
-            t1 = -initial_velocity / acceleration + sqrt(2 * acceleration * distance + initial_velocity ** 2) / acceleration
-            t2 = -initial_velocity / acceleration - sqrt(2 * acceleration * distance + initial_velocity ** 2) / acceleration
-            print("")
-            print("Final velocity =\n", final_velocity, "\nor ", -final_velocity)
-            print("Time =\n", t1, "\nor ", t2)
+    elif variables['distance'] and variables['final_velocity'] and variables['time']:
+        variables['initial_velocity'] = 2 * variables['distance'] / variables['time'] - variables['final_velocity']
+        variables['acceleration'] = (variables['final_velocity'] - variables['initial_velocity']) / variables['time']
+        print("Initial velocity =\n", variables['initial_velocity'])
+        print("Acceleration =\n", variables['acceleration'])
 
-        elif s_known and u_known and t_known:
-            final_velocity = 2 * distance / time - initial_velocity
-            acceleration = 2 * (distance - initial_velocity * time) / time ** 2
-            print("")
-            print("Final velocity =\n", final_velocity)
-            print("Acceleration =\n", acceleration)
+    elif variables['distance'] and variables['acceleration'] and variables['time']:
+        variables['initial_velocity'] = (variables['distance'] - 0.5 * variables['acceleration'] * variables['time'] ** 2) / variables['time']
+        variables['final_velocity'] = variables['initial_velocity'] + variables['acceleration'] * variables['time']
+        print("Initial velocity =\n", variables['initial_velocity'])
+        print("Final velocity =\n", variables['final_velocity'])
 
-        elif s_known and v_known and a_known:
-            initial_velocity = sqrt(final_velocity ** 2 - 2 * acceleration * distance)
-            t1 = 2 * distance / (final_velocity + sqrt(final_velocity ** 2 - 2 * acceleration * distance))
-            t2 = 2 * distance / (final_velocity - sqrt(final_velocity ** 2 - 2 * acceleration * distance))
-            print("")
-            print("Initial velocity =\n", initial_velocity, "\nor", -initial_velocity)
-            print("Time =\n", t1, "\nor", t2)
+    elif variables['initial_velocity'] and variables['final_velocity'] and variables['acceleration']:
+        variables['distance'] = (variables['final_velocity'] ** 2 - variables['initial_velocity'] ** 2) / (2 * variables['acceleration'])
+        variables['time'] = (variables['final_velocity'] - variables['initial_velocity']) / variables['acceleration']
+        print("Distance =\n", variables['distance'])
+        print("Time =\n", variables['time'])
 
-        elif s_known and v_known and t_known:
-            initial_velocity = 2 * distance / time - final_velocity
-            acceleration = (final_velocity - initial_velocity) / time
-            print("")
-            print("Initial velocity =\n", initial_velocity)
-            print("Acceleration =\n", acceleration)
+    elif variables['initial_velocity'] and variables['final_velocity'] and variables['time']:
+        variables['distance'] = 0.5 * (variables['initial_velocity'] + variables['final_velocity']) * variables['time']
+        variables['acceleration'] = (variables['final_velocity'] - variables['initial_velocity']) / variables['time']
+        print("Distance =\n", variables['distance'])
+        print("Acceleration =\n", variables['acceleration'])
 
-        elif s_known and a_known and t_known:
-            initial_velocity = (distance - 0.5 * acceleration * time ** 2) / time
-            final_velocity = initial_velocity + acceleration * time
-            print("")
-            print("Initial velocity =\n", initial_velocity)
-            print("Final velocity =\n", final_velocity)
+    elif variables['initial_velocity'] and variables['acceleration'] and variables['time']:
+        variables['final_velocity'] = variables['initial_velocity'] + variables['acceleration'] * variables['time']
+        variables['distance'] = variables['initial_velocity'] * variables['time'] + 0.5 * variables['acceleration'] * variables['time'] ** 2
+        print("Final velocity =\n", variables['final_velocity'])
+        print("Distance =\n", variables['distance'])
 
-        elif u_known and v_known and a_known:
-            distance = (final_velocity ** 2 - initial_velocity ** 2) / (2 * acceleration)
-            time = (final_velocity - initial_velocity) / acceleration
-            print("")
-            print("Distance =\n", distance)
-            print("Time =\n", time)
+    elif variables['final_velocity'] and variables['acceleration'] and variables['time']:
+        variables['initial_velocity'] = variables['final_velocity'] - variables['acceleration'] * variables['time']
+        variables['distance'] = 0.5 * (2 * variables['final_velocity'] - variables['acceleration'] * variables['time']) * variables['time']
+        print("Initial velocity =\n", variables['initial_velocity'])
+        print("Distance =\n", variables['distance'])
 
-        elif u_known and v_known and t_known:
-            distance = 0.5 * (initial_velocity + final_velocity) * time
-            acceleration = (final_velocity - initial_velocity) / time
-            print("")
-            print("Distance =\n", distance)
-            print("Acceleration =\n", acceleration)
 
-        elif u_known and a_known and t_known:
-            final_velocity = initial_velocity + acceleration * time
-            distance = initial_velocity * time + 0.5 * acceleration * time ** 2
-            print("")
-            print("Final velocity =\n", final_velocity)
-            print("Distance =\n", distance)
-
-        elif v_known and a_known and t_known:
-            initial_velocity = final_velocity - acceleration * time
-            distance = 0.5 * (2 * final_velocity - acceleration * time) * time
-            print("")
-            print("Initial velocity =\n", initial_velocity)
-            print("Distance =\n", distance)
-
-        print("")
-        break
-
+print("Suvat Calculator:")
+print("if variable not\ngiven then type\n'x'\n")
 while True:
     suvatsolver()
     stopper()
+    print()
